4515ed7d2178e90bbf2e9d24e927df9a1f4d320b
Despina Zografidou <87451544+DespoinaZografidou@users.noreply.github.com>
2021-07-31 01:43:25 +0300

    Update Exercise21.c

--- Exercise21.c
+++ Exercise21.c
@@ -1,55 +1,56 @@
-/*Μέλη Ομάδας: Χατζηβασίλη Νίκη, 321/2016215
-               Ζωγραφίδου Δέσποινα, 321/2016041 */
+/*Despoina Zografidou, 321/2016041 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//anadromhkh sunarthsh gia to athroisma pshfiwn
+//recursive function for sum of digits
 int recursion_sum(int N){
 
-    if(N == 0) //periptwsh pou o arithmos einai 0
-        return 0; //epistrefei 0
+    if(N == 0) //in case that the integer is equal with 0
+        return 0; //return 0
     else
         return (N%10 + recursion_sum(N/10));
-        //xrhsimopoihsame to mod 10 gia na broume posa stoixeia exoume
-        //sunthiki termatismou: otan N=0 (den exw alla stoixeia)
+        
 }
 
-//anadromhkh sunarthsh gia ton elegxo tajinomhshs stoixeiwn se aujousa seira
-//Xronikh Poluplokothta: O(1), den exw epanalipseis
+
+//recursive function to check if the table is sorted by ascending order
 int recursion_sort(int A[], int N, int i){
 
-    //sunthiki termatismou: otan ftasei sto telos tou pinaka kai einai taksinomimenos
+    //if you checked all the elements in the table then return that the table is sorted
     if (i == 0)
         return 1;
-
-    else if(A[i-1] > A[i]) //an to stoixeio ths prohgoumenhs thesh einai megalytero apo thn epomenh
-        return 0;   //o pinakas den einai taksinomhmenos
-
+  
+    //if A[i-1] > A[i] then the table is not sorted
+    else if(A[i-1] > A[i]) 
+        return 0;   
+    //else continue to the next element
     else
         return recursion_sort(A, N, i-1);  //alliws proxoraei stis prohgoymenes theseis
 }
 
-//sunarhsh duadikhs anazhthshs
+//recursive function for the Binary Search
 int binary_search(int A[], int left, int right, int key)//diadikh anazhthsh se tajinomhmeno pinaka
 {
     int mid;
 
     while (left <= right){
 
-    mid = (left+right)/2; //mesaio stoixeio tou pinaka
-
-    if (key == A[mid]) //periptwsh pou vrei to stoixeio
+    mid = (left+right)/2; 
+    
+    //if there is an integer that is equal with the key then return the position  
+    if (key == A[mid]) 
         return mid;
-
+    
+    //if the key is bigger than the A[mid]   
     else if (key > A[mid])
         return binary_search(A, mid+1, right, key);
-
+    //if the key is smaller than the A[mid]  
     else
         return binary_search(A, left, mid-1, key);
     }
-    //periptwsh pou den uparxei to stoixeio
+    //in case that there is not any element that is equal with the key
     return -1;
 }
 
@@ -66,19 +67,19 @@ int main(){
     scanf("%d", &choice);
 
     if(choice == 1 || choice == 2 || choice == 3){
-//Askhsh 1
+
         if(choice == 1){
             int num;
             int function;
 
             printf("\nPlease insert an integer: ");
-            scanf("%d", &num); //eisagei aritmo o xrhsths
+            scanf("%d", &num); 
 
-            function = recursion_sum(num); //kalesma anadromhkhs sunartishs
+            function = recursion_sum(num); 
 
             printf("Sum of digits: %d \n", function);
         }
-//Askhsh 2
+
         else if(choice == 2){
             int *A;
             int N;
@@ -87,9 +88,8 @@ int main(){
             printf("\nInsert the size of the array: ");
             scanf("%d",&N);
 
-            A = (int*) malloc (N * sizeof(int)); //desmeush mnhmhs gia thesh pinaka
-
-            //gemisma pinaka apo xrhsth
+            A = (int*) malloc (N * sizeof(int)); 
+ 
             for(int i=0; i<N; i++){
                 printf("Insert the %d integer: ", i);
                 scanf("%d", &A[i]);
@@ -97,19 +97,18 @@ int main(){
 
             printf("\nTable: ");
 
-            //tupwnei pinaka
             for(int j=0; j<N; j++){
                 printf("%d ", A[j]);
             }
 
-            function1 = recursion_sort(A, N, N-1); //kalesma sunarthshs
+            function1 = recursion_sort(A, N, N-1); 
 
             if(function1 == 1)
                 printf("\nThe array is sorted.\n");
             else
                 printf("\n\nThe array is not sorted!\n");
         }
-//Askhsh 3
+
         else if(choice == 3){
             int A[]= {2,4,6,8,10,12,14,16,18};
             int key;
@@ -117,7 +116,7 @@ int main(){
 
             printf("\nTable: ");
 
-            //tupwnei pinaka
+           
             for(int i=0; i<9; i++){
                 printf("%d ",A[i]);
             }
@@ -125,7 +124,7 @@ int main(){
             printf("\n\nInsert an integer to search in the array: ");
             scanf("%d", &key);
 
-            function2 = binary_search(A, 0, 9, key); //kalesma sunarthshs
+            function2 = binary_search(A, 0, 9, key); 
 
             if (function2 == -1)
                 printf("This element does not exist!");
@@ -136,7 +135,7 @@ int main(){
     }
     else{
         printf("Error!");
-        return -1; //epistrefei -1
+        return -1; 
     }
 }
 
